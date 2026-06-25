ef80befde9249e68226bb68fb6f9336a471d8d2b
finalSay9 <chimwazaevan1@gmail.com>
2026-06-25 09:22:01 +0200

    learning sckilearn

--- practice.py
+++ practice.py
@@ -1,8 +1,12 @@
 from sklearn.linear_model import LinearRegression
 
+# a 4 by 1 matrix (that is 4 rows, 1 column)
 X = [[1], [2], [3], [4]]   # input
+
+# this is a 1 by 4 matrix, target vector
 y = [30, 40, 50, 60]       # output
 
+#
 model = LinearRegression()
 model.fit(X, y)
 
