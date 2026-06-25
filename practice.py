6ce8586e6b73f62279c833176bc18c4f5e085f3c
finalSay9 <chimwazaevan1@gmail.com>
2026-06-25 09:26:22 +0200

    trying to train the models

--- practice.py
+++ practice.py
@@ -6,8 +6,10 @@ X = [[1], [2], [3], [4]]   # input
 # this is a 1 by 4 matrix, target vector
 y = [30, 40, 50, 60]       # output
 
-#
+#initializing an empty model(creating a blank linear regression object)
 model = LinearRegression()
+
+#now need to train the model
 model.fit(X, y)
 
 print(model.coef_)     # this is m
