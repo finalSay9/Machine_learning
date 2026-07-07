c2aeac0ab9550ac1151b64c2dcda1230c067658a
finalSay9 <chimwazaevan1@gmail.com>
2026-07-07 15:48:35 +0200

    some models

--- premierLegue.py
+++ premierLegue.py
@@ -1,3 +1,10 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
+
+
+#loading the csv into pandas dataframe
+df = pd.read_csv('games.csv')
+
+#now select your Feature matrix (X) and target vector y
+X = df[[]]
