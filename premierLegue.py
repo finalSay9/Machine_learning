8b8c3a93a847cca36832c1ab20934c6fa5947b9c
finalSay9 <chimwazaevan1@gmail.com>
2026-07-07 15:55:56 +0200

    some models

--- premierLegue.py
+++ premierLegue.py
@@ -7,4 +7,17 @@ from sklearn.linear_model import LinearRegression
 df = pd.read_csv('games.csv')
 
 #now select your Feature matrix (X) and target vector y
-X = df[[]]
+X = df[['games_played', 'games_won', 'games_draw', 'games_lost']]
+
+#now lets come to y(predicted value) and should be 1D
+y = df['total_points']
+
+#oky now lets initialize the empty model
+model = LinearRegression()
+
+#train the model
+model.fit(X, y)
+
+# 4. Look at the math the model learned
+print("Intercept (c):", model.intercept_)
+print("Coefficients (m for each feature):", model.coef_)
