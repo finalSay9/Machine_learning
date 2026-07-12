774a2a1326adf73812ccdcbb15d662b247e98b96
finalSay9 <chimwazaevan1@gmail.com>
2026-07-12 15:40:05 +0200

    creating and learning logisitc egression

--- LogisticRegression/exams.py
+++ LogisticRegression/exams.py
@@ -5,5 +5,7 @@ import numpy as np
 
 #school data
 school_data = pd.DataFrame({
+    'score': [40,44,66,77,90,11,70,90,88,55,66,77,88,99],
+    'pass': [0,0,1,1,1,0,1,1,1,0,1,1,1,1]
+    })
     
-})
