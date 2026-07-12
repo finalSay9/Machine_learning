ec4f07ff201172f22ea7c741a598b29cfbc85e4e
finalSay9 <chimwazaevan1@gmail.com>
2026-07-12 15:48:37 +0200

    creating and learning logisitc egression

--- LogisticRegression/exams.py
+++ LogisticRegression/exams.py
@@ -8,4 +8,13 @@ school_data = pd.DataFrame({
     'score': [40,44,66,77,90,11,70,90,88,55,66,77,88,99],
     'pass': [0,0,1,1,1,0,1,1,1,0,1,1,1,1]
     })
-    
+
+model = LogisticRegression()
+model.fit(school_data[['score']], school_data['pass']) 
+
+#predict the exact category
+
+new_score = [[35],[60],[90],[9]]
+predictions = model.predict(new_scores)
+
+print("Predicted Outcomes (0=Fail, 1=Pass):", predictions)
