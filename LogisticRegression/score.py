3b05d7aa8437ff84edcba659eda51ce5839faf64
finalSay9 <chimwazaevan1@gmail.com>
2026-07-16 17:41:59 +0200

    creating the dataset table for all these

--- LogisticRegression/score.py
+++ LogisticRegression/score.py
@@ -2,5 +2,7 @@ import pandas as pd
 
 
 
-data = pd.read_csv('student.csv')
-print(data.describe())
+data = pd.read_csv('../datasets/student.csv')
+
+placement_status  = data['placement_status'].value_counts()
+print(data.info())
