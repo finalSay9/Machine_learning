b360b20ad6d23b8c5476c21015931b3810e05ff8
finalSay9 <chimwazaevan1@gmail.com>
2026-07-16 17:49:34 +0200

    creating the dataset table for all these

--- LogisticRegression/score.py
+++ LogisticRegression/score.py
@@ -1,8 +1,14 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 
 data = pd.read_csv('../datasets/student.csv')
 
 placement_status  = data['placement_status'].value_counts()
+
+plots = data.hist(bins=50, figsize=(20,15))
+plt.show()
 print(data.info())
+print(placement_status)
+print(data.describe())
