edbf116628481e3d49e5a589a9a477ced3abdd89
finalSay9 <chimwazaevan1@gmail.com>
2026-07-17 08:29:03 +0200

    adding the test set for/rather seting aside the test set

--- LogisticRegression/score.py
+++ LogisticRegression/score.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 
 data = pd.read_csv('../datasets/student.csv')
 
 placement_status  = data['placement_status'].value_counts()
+
+#setting aside the test set
+def
+
 
 plots = data.hist(bins=50, figsize=(20,15))
 plt.show()
