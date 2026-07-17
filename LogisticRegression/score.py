e63aa82a35d43f28c7c7c320f71f40619fec424a
finalSay9 <chimwazaevan1@gmail.com>
2026-07-17 08:35:36 +0200

    the test set

--- LogisticRegression/score.py
+++ LogisticRegression/score.py
@@ -10,7 +10,11 @@ placement_status  = data['placement_status'].value_counts()
 
 #setting aside the test set
 def split_train_test(data, test_ratio):
-    shuffled_indeces = np.random.
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
 
 
 plots = data.hist(bins=50, figsize=(20,15))
