bb7eb88fb37a5ce2216e9ad08dc080e6f231efc5
finalSay9 <chimwazaevan1@gmail.com>
2026-08-06 08:31:35 +0200

    its a 3 X 3 matrix

--- deep-learning/tensor.py
+++ deep-learning/tensor.py
@@ -1,18 +1,28 @@
 import numpy as np
 
 inputs = [
-    [1,-2,1.0,1.9],
-    [2.1,0,1.1,3],
-    [0.1,-3,5,1.0],
+    [1, -2, 1.0, 1.9],
+    [2.1, 0, 1.1, 3],
+    [0.1, -3, 5, 1.0],
    
     ]
 
 weights = [
-    [0.1,0.2,-1,0.5],
-    [1.0,0.3,0.1,1.1],
-    [1,3,4,5],
+    [0.1, 0.2, -1, 0.5],
+    [1.0, 0.3, 0.1, 1.1],
+    [1, 3, 4, 5],
     
 ]
-weights = np.array(weights).T
-calc = np.dot(inputs, weights)
-print(calc)
+bias1 = [1, 2, 3]
+
+weight1 = [
+    [1, 2,  3.1],
+    [1.0,  0.1, 0.0],
+    [2, 3,  0]
+    ]
+
+biase1 = [0.1, 3.1, 2.0]
+
+dot1 = np.dot(inputs, np.array(weights).T) + bias1
+dot2 = np.dot(dot1, np.array(weight1).T) + biase1
+print(dot2)
