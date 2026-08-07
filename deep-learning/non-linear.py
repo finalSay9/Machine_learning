613f4caca4090075b4001a44ad441da6bf02afce
finalSay9 <chimwazaevan1@gmail.com>
2026-08-07 09:31:51 +0200

    its a 3 X 3 matrix

--- deep-learning/non-linear.py
+++ deep-learning/non-linear.py
@@ -5,6 +5,12 @@ import random
 class Dense_layer:
     def __init__(self, n_inputs, n_nuerons):
         #initialize weights and biases
+        #Guassian distribution
+        """
+        n_inputs represent the number of rows and
+        the n_neurons represent the number of columns in the
+        weight matrix
+        """
         self.weights = 0.01 * np.random.randn(n_inputs, n_nuerons)
         self.biases = np.zeros((1, n_nuerons))
         
