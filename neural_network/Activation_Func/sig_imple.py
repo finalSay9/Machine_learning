2da1ad6e81424152872ce59a34d11e04c2de7d28
finalSay9 <chimwazaevan1@gmail.com>
2026-08-31 15:43:45 +0200

    creating the activation fucntions

--- neural_network/Activation_Func/sig_imple.py
+++ neural_network/Activation_Func/sig_imple.py
@@ -3,8 +3,22 @@ import Sigmod_fun
 
 
 class SigmImplementation(Sigmod_fun):
+
+   #class attributes
+    inputs = [
+        [1, -2, 1.0, 1.9],
+        [2.1, 0, 1.1, 3],
+        [0.1, -3, 5, 1.0],
     
-    def __init__(self, inputs, weights, biases):
-        self.inputs = inputs
-        self.weights = weights
-        self.biases = biases
+        ]
+
+    weights = [
+        [0.1, 0.2, -1, 0.5],
+        [1.0, 0.3, 0.1, 1.1],
+        [1, 3, 4, 5],
+        
+    ]
+    bias1 = [1, 2, 3]
+
+    def __init__(self):
+     pass
