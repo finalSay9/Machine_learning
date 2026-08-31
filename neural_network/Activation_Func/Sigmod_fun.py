aaa91c2fd282436721d1c64b714a4e072348f013
finalSay9 <chimwazaevan1@gmail.com>
2026-08-31 15:33:34 +0200

    creating the activation fucntions

--- neural_network/Activation_Func/Sigmod_fun.py
+++ neural_network/Activation_Func/Sigmod_fun.py
@@ -12,3 +12,9 @@ class Sigmod():
             1 / (1 + np.exp(-value)),
             np.exp(value) / (1 + np.exp(value))
         )
+
+
+    def sig_backward(self, value1):
+        #Derivative: f(x) - (1 - f(x))
+        value1 = self.sig_forward(value1)
+        return value1 * (1 - value1)
