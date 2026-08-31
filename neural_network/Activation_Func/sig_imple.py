b3b60f17df7ff1c6d69b657b31d14a3c65774cee
finalSay9 <chimwazaevan1@gmail.com>
2026-08-31 15:38:56 +0200

    creating the activation fucntions

--- neural_network/Activation_Func/sig_imple.py
+++ neural_network/Activation_Func/sig_imple.py
@@ -1,1 +1,10 @@
 import numpy as np
+import Sigmod_fun
+
+
+class SigmImplementation(Sigmod_fun):
+    
+    def __init__(self, inputs, weights, biases):
+        self.inputs = inputs
+        self.weights = weights
+        self.biases = biases
