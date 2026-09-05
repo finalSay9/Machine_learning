5337a9c97ed9eab0b81f0c3641d9fb6f5f97b9e7
finalSay9 <chimwazaevan1@gmail.com>
2026-09-05 08:04:13 +0200

    numpy playing with the ndarray

--- numpy/basics.py
+++ numpy/basics.py
@@ -9,3 +9,12 @@ print(data.shape)
 print(data.ndim)
 print(data.itemsize)
 
+"""
+creation of an array in numpy
+sequences of sequences of sequences
+"""
+
+data_1 = np.array([1,2,3,4,5],
+                  [6,7,8,9,0],
+                  [9,0,7,1])
+print(data_1)
