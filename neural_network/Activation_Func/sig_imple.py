5103b472563f0ac4d1d8bfa2fe417d595184a992
finalSay9 <chimwazaevan1@gmail.com>
2026-09-05 07:44:26 +0200

    dto creating here

--- neural_network/Activation_Func/sig_imple.py
+++ neural_network/Activation_Func/sig_imple.py
@@ -1,23 +1,18 @@
 import numpy as np
 import Sigmod_fun
+from Sigmod_fun import Sigmod
 
 
-class SigmImplementation(Sigmod_fun):
+class SigmImplementation(Sigmod):
 
    #class attributes
-    inputs = [
-        [1, -2, 1.0, 1.9],
-        [2.1, 0, 1.1, 3],
-        [0.1, -3, 5, 1.0],
-    
-        ]
+    inputs = [1, -2, 1.0, 1.9],
+      
 
-    weights = [
-        [0.1, 0.2, -1, 0.5],
-        [1.0, 0.3, 0.1, 1.1],
-        [1, 3, 4, 5],
+    weights = [0.1, 0.2, -1, 0.5],
         
-    ]
+        
+    
     bias1 = [1, 2, 3]
     sum = 0
 
@@ -27,7 +22,11 @@ class SigmImplementation(Sigmod_fun):
     #the forward passs
     def forward(self):
        for i in zip(self.inputs, self.weights):
-          result = self.inputs[i] * self.weights
+          result = self.inputs[i] * self.weights[i]
           sum += result
+          super().sig_forward(sum)
           return sum
-          
+
+
+obj1 = SigmImplementation()
+print(obj1.forward())         
